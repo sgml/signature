8f8d31b1807134f0e0ea06b1847e3185e24be1d3
sgml <paul.sweatte@etrade.com>
2018-06-14 20:57:42 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,13 @@
  * @todo add todo list
  * @todo execute todo list
  */
+
+/**
+ * @desc ##@desc is the description annotation
+ */
+
+/**
+ * @example
+ * const result = RegExp("\\d+").exec(7779311);
+ * console.log(result);
+ */
